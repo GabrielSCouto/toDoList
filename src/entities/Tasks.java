2e484d1c96904e8f946cb9c776205ebda8649f1c
gabrielcouto <goldevil2015@gmail.com>
2024-12-18 12:56:55 -0300

    feat: add class 'Tasks' with 'add' and 'show' methods

---
+++ src/entities/Tasks.java
@@ -0,0 +1,89 @@
+package entities;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Tasks {
+    private String taskName;
+    private String taskDescription;
+    private String taskDate;
+
+    static List<Tasks> tasks = new ArrayList<>();
+
+    static Scanner sc = new Scanner(System.in);
+
+    public Tasks(){
+
+    }
+
+    public Tasks(String taskName, String taskDescription, String taskDate) {
+        this.taskName = taskName;
+        this.taskDescription = taskDescription;
+        this.taskDate = taskDate;
+    }
+
+    public Tasks(String taskName, String taskDescription) {
+        this.taskName = taskName;
+        this.taskDescription = taskDescription;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public String getTaskDescription() {
+        return taskDescription;
+    }
+
+    public String getTaskDate() {
+        return taskDate;
+    }
+
+    public static void addTask(){
+
+        System.out.println("Enter task data:");
+        System.out.print("TITLE: ");
+        String taskName = sc.nextLine();
+        System.out.print("DESCRIPTION: ");
+        String taskDescription = sc.nextLine();
+        System.out.print("Wish to add deadline to current task? (y/n) ");
+        String option = sc.nextLine().trim().toLowerCase();
+
+        do {
+            switch (option){
+                case "y":
+                    System.out.println("DEADLINE (yyyy-mm-dd): ");
+                    String taskDate = sc.nextLine();
+                    tasks.add(new Tasks(taskName, taskDescription, taskDate));
+                    break;
+                case "n":
+                    tasks.add(new Tasks(taskName, taskDescription));
+                    break;
+                default:
+                    System.out.println("Invalid entry, try again!");
+                    break;
+            }
+        } while (!option.equalsIgnoreCase("y") && !option.equalsIgnoreCase("n"));
+
+    }
+
+    public void removeTask(){
+
+    }
+
+    public void updateTask(){
+
+    }
+
+    public static void showTasks(){
+        System.out.println("Showing tasks: ");
+        for (Tasks task : tasks) {
+            System.out.println(task);
+        }
+    }
+
+    public String toString(){
+        return "Title: " + taskName + ", Description: " + taskDescription + ", Deadline: " + taskDate;
+    }
+}
--- src/menus/MainMenu.java
+++ src/menus/MainMenu.java
@@ -1,5 +1,7 @@
 package menus;
 
+import entities.Tasks;
+
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Scanner;
@@ -8,25 +10,37 @@ public class MainMenu {
 
     public static void menu() {
         Scanner sc = new Scanner(System.in);
+        System.out.println("|-----------------------|");
+        System.out.println("|NEXT 7 SEVEN DAYS TASKS|");
+        System.out.println("|-----------------------|");
+
+        //display here the next week tasks
 
 
         while (true) {
-            System.out.println("\n--- MENU ---");
+            System.out.println("\n------------------");
             System.out.println("1. Add new task");
             System.out.println("2. View all tasks");
             System.out.println("3. Update a task");
             System.out.println("4. Delete a task");
-            System.out.println("5. Exit");
+            System.out.println("0. Exit");
+            System.out.println("------------------");
+            System.out.println();
             System.out.print("Choose an option: ");
             int option = sc.nextInt();
             sc.nextLine();
 
             switch (option) {
-                case 5 -> {
+                case 1:
+                    entities.Tasks.addTask();
+                    break;
+                case 2:
+                    Tasks.showTasks();
+                case 0:
                     System.out.println("Exiting...");
                     return;
-                }
-                default -> System.out.println("Invalid option. Try again.");
+
+                default: System.out.println("Invalid option. Try again.");
             }
         }
     }
